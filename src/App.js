0d9132483652ae6e18bfbfadd82e14641230763a
Kameron Comer <kam.comer96@gmail.com>
2022-04-18 23:23:37 -0400

    Added content tag to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import NavigationBar from "./components/Layout/NavigationBar";
+import Menu from "./components/Layout/Menu";
+import image from "./assets/images/sushi.jpg";
 
 function App() {
   return (
     <div className="App">
+      <img src={image} />
       <NavigationBar />
+      <div className="content">
+        <Menu />
+      </div>
     </div>
   );
 }
