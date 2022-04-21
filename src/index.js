d790d0e8ebd504a6d9742f278a1f3ef7dc4e90bb
Kameron Comer <kam.comer96@gmail.com>
2022-04-20 23:01:00 -0400

    Included App component into the scope of a cart context provider component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import CartCtxProvider from "./components/Cart/CartCtxProvider";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
+  <CartCtxProvider>
     <App />
+  </CartCtxProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
