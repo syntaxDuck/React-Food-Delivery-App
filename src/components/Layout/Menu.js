eeeee4f17700ea96dd4fea897e9bfa924609cc15
Kameron Comer <kam.comer96@gmail.com>
2022-04-24 23:58:49 -0400

    Began the implementation of reteriving and posting data to project database

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -5,70 +5,110 @@ import classes from "./Menu.module.css";
 import Button from "../UI/Button";
 import CartContext from "../Cart/cart-context";
 
+//import project specific variables
+import { PROJECT_ID } from "../../private/PRIVATE";
+
 const Menu = () => {
   console.log("Rendering Menu");
+  const cartCtx = React.useContext(CartContext);
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
 
-  const menuItems = [
-    {
-      id: "i1",
-      name: "california roll",
-      description: "a sushi roll",
-      price: 10,
-    },
-    {
-      id: "i2",
-      name: "angus burger",
-      description: "moooooooooooooooooooo",
-      price: 12,
-    },
-    {
-      id: "i3",
-      name: "romen",
-      description: "shit thrown into a pot... pretty good",
-      price: 8,
-    },
-  ];
+  const dbUrl = 'https://' + [PROJECT_ID] + ".firebaseio.com/";
 
-  const { updateCart } = React.useContext(CartContext);
-  let items = [];
+
+  // Retreive menu data from database
+  React.useEffect(() => {
+    const getMenu = async () => {
+      try {
+        const response = await fetch(dbUrl + "Menu.json", {
+          method: "GET",
+        });
+
+        console.log(JSON.stringify(response.json));
+
+        if (!response.status) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+
+        let actualData = await response.json();
+
+        setData(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMenu();
+  }, []);
+  
+  let order = [];
 
   const addToPreCartHandler = (newItem) => {
-    const existingCartItemIndex = items.findIndex(
+    const existingCartItemIndex = order.findIndex(
       (item) => item.id === newItem.id
     );
-    const existingCartItem = items[existingCartItemIndex];
+    const existingCartItem = order[existingCartItemIndex];
     if (existingCartItem) {
       const updatedItem = {
-        ...items[existingCartItemIndex],
+        ...order[existingCartItemIndex],
         amount: newItem.amount,
       };
-      items[existingCartItemIndex] = updatedItem;
+      order[existingCartItemIndex] = updatedItem;
     } else {
-      items.push(newItem);
+      order.push(newItem);
     }
   };
 
   const updateCartHandler = (event) => {
     event.preventDefault();
-    updateCart(items);
+    const postRequest = async () => {
+      const post = dbUrl + "/" + "orders.json";
+      console.log(post);
+      const response = await fetch(post, {
+        method: "POST",
+        headers: { contentType: "application/json" },
+        body: JSON.stringify(order),
+      });
+      return response;
+    };
+
+    const response = postRequest();
+    console.log(response);
+  };
+
+  if (error) console.log(error);
+
+  // Create Menu item components if data was retreived from database
+  let menuContent;
+  if (loading) {
+    menuContent = <p>Loading...</p>;
+  } else {
+    menuContent = (
+      <ul className={classes["menu-items"]}>
+        {Object.keys(data).map((menuItem) => {
+          return (
+            <MenuItem
+              key={data[menuItem].id}
+              id={data[menuItem].id}
+              price={data[menuItem].price}
+              name={menuItem}
+              description={data[menuItem].description}
+              onAddToPreCart={addToPreCartHandler}
+            />
+          );
+        })}
+      </ul>
+    );
   }
 
-  const menuContent = (
-    <ul className={classes["menu-items"]}>
-      {menuItems.map((menuItem) => {
-        return (
-          <MenuItem
-            key={menuItem.id}
-            id={menuItem.id}
-            price={menuItem.price}
-            name={menuItem.name}
-            description={menuItem.description}
-            onAddToPreCart={addToPreCartHandler}
-          />
-        );
-      })}
-    </ul>
-  );
   return (
     <Card className={classes["menu-card"]}>
       <form onSubmit={updateCartHandler}>
