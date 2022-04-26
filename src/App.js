5418729e1bdb04c56444fe63a6ad3da2a8d0407f
Kameron Comer <kam.comer96@gmail.com>
2022-04-25 23:26:24 -0400

    Added cart to app render and passed handler function to cart related compnenets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import NavigationBar from "./components/Layout/NavigationBar";
 import Menu from "./components/Layout/Menu";
+import Cart from "./components/Cart/Cart";
 import image from "./assets/images/sushi.jpg";
+import React from "react";
 
 function App() {
+  const [cartActive,setCartActive] = React.useState(false);
+
+  const cartStateHandler = () => {
+    setCartActive((prevState) => !prevState);
+  }
+
   console.log("Rendering");
   return (
       <div className="App">
+        {cartActive && <Cart onCartStateChange={cartStateHandler}/>}
         <img src={image} alt="Sushi"/>
-        <NavigationBar />
+        <NavigationBar onCartStateChange={cartStateHandler}/>
         <div className="content">
           <Menu />
         </div>
