04827f5b21a1de8a2364c43bc6a77b637706fdf7
Kameron Comer <kam.comer96@gmail.com>
2022-04-26 22:44:12 -0400

    Added cart amount component for pre order submit changes

---
+++ src/components/Cart/CartItemAmount.js
@@ -0,0 +1,50 @@
+import React from "react";
+import classes from "./CartItemAmount.module.css";
+
+const CartItemAmount = ({amountInCart, onUpdateCart}) => {
+
+  const [amount, setAmount] = React.useState(amountInCart);
+  const isMounted = React.useRef(false);
+
+  React.useEffect(() => {
+    //Logic help avoid double render on mounting
+    if(isMounted.current) setAmount(0);
+    else isMounted.current=true;
+  }, [onUpdateCart]);
+
+  const incrementCountHandler = () => {
+    setAmount((prevState) => {
+      onUpdateCart(prevState + 1);
+      return prevState + 1;
+    });
+  };
+
+  const decrementCountHandler = () => {
+    setAmount((prevState) => {
+      onUpdateCart(prevState - 1);
+      return prevState - 1;
+    });
+  };
+
+  return (
+    <div className={classes["item-count"]}>
+      <button
+        type="button"
+        onClick={incrementCountHandler}
+        disabled={amount === 99}
+      >
+        +
+      </button>
+      <label>{amount}</label>
+      <button
+        type="button"
+        onClick={decrementCountHandler}
+        disabled={amount === 0}
+      >
+        -
+      </button>
+    </div>
+  );
+};
+
+export default CartItemAmount;
