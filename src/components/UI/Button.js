fad3f69ab0920dff460d31bf7287fa1301f36b4c
Kameron Comer <kam.comer96@gmail.com>
2022-05-06 00:51:08 -0400

    Added anchor tag functionality to button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,16 +1,14 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-const button = ({ className, onClick, disabled, label }) => {
+const button = ({ href, className, onClick, disabled, label }) => {
   const buttonClasses = `${className || classes["button-default"]}`;
 
   return (
-    <button
-      className={buttonClasses}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      <p>{label || "button"}</p>
+    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
+      <a className={classes.anchor} href={href}>
+        <p href={href}>{label || "button"}</p>
+      </a>
     </button>
   );
 };
