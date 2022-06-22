dbbae044c105ef7dc95be7516a2707176b99f30c
Kameron Comer <kam.comer96@gmail.com>
2022-06-21 23:11:49 -0400

    Added ability to customize button type via props

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-const button = ({ href, className, onClick, disabled, label }) => {
+const button = ({ href, className, onClick, disabled, label, type}) => {
   const buttonClasses = `${className || classes["button-default"]}`;
 
   return (
-    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
+    <button className={buttonClasses} type={type} onClick={onClick} disabled={disabled}>
       <a className={classes.anchor} href={href}>
         <p href={href}>{label || "button"}</p>
       </a>
